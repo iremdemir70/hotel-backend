26ae478666202b90715fa16b5b48c1c6a13ae56f
irem demir <iremdemir70@hotmail.com>
2025-06-28 10:35:05 +0300

    Backend initial commit

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+
+def create_app():
+    load_dotenv()
+
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    CORS(app)
+
+    @app.route('/')
+    def home():
+        return "Backend çalışıyor 🟢"
+
+    return app
