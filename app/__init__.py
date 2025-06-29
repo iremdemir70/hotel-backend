1945d77aa9fd10eb7c392f9225b65246856bd19a
irem demir <iremdemir70@hotmail.com>
2025-06-30 00:10:14 +0300

    cors

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,11 @@ def create_app():
 
     app = Flask(__name__)
 
-    CORS(app, origins="http://localhost:4200", supports_credentials=True)
+    CORS(app, origins=[
+    "http://localhost:4200", 
+    "https://hotel-frontend-lemon.vercel.app"
+], supports_credentials=True)
+
 
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
